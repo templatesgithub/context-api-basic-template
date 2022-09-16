02a8b5330652fd2d9ed614f09869453f88138a47
jorgesehma <jorge.alberto@sehma.com>
2022-09-16 09:51:22 -0400

    adding final fixes

--- src/context/testState.js
+++ src/context/testState.js
@@ -1,20 +1,27 @@
 import React, { useReducer } from "react";
 import testReducer from "./testReducer";
 import testContext from "./testContext";
+import PropTypes from "prop-types";
 
-const TestState = () => {
-
+const TestState = ({ children }) => {
 	const initialState = {
 		text: "Hello"
-	}
+	};
 
+	// eslint-disable-next-line no-unused-vars
 	const [state, dispatch] = useReducer(testReducer, initialState);
 
 	return (
-		<testContext.Provider value={{}}>
+		<testContext.Provider value={{
+			text: state.text
+		}}>
 			{ children}
 		</testContext.Provider>
 	);
+};
+
+TestState.propTypes = {
+	children: PropTypes.node.isRequired
 };
 
 export default TestState;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import TestState from "./context/testState";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<>
-		<App />
-	</>
+	<StrictMode>
+		<TestState>
+			<App />
+		</TestState>
+	</StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
